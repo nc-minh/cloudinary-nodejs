ff8fb6d044a76dab2dcfedc8f38ae63c063cf761
nc-minh <nguyencongminh.official@gmail.com>
2022-01-13 06:58:36 +0700

    :bento: update logic client

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,29 @@ function getBase64(file) {
 }
 
 function upload() {
-    var file = document.getElementById('file')
+    var file = document.getElementById('file-upload')
+    var _file = file.files[0]
+
+    getBase64(_file).then(data => {
+
+        fetch('/api/upload', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                data
+            })
+        })
+            .then(response => response.json())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error))
+    })
+
+}
+
+function uploadVideo() {
+    var file = document.getElementById('file-upload-video')
     var _file = file.files[0]
 
     getBase64(_file).then(data => {
@@ -27,4 +49,40 @@ function upload() {
             .catch(error => console.log('error', error))
     })
 
+}
+
+function deleteImage(){
+    var id = document.getElementById('text-delete').value
+
+    fetch('/api/delete', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "id": id
+            }),
+            redirect: 'follow'
+        })
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error))
+}
+
+function deleteVideo(){
+    var id = document.getElementById('text-delete-video').value
+
+    fetch('/api/delete-video', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "id": id
+            }),
+            redirect: 'follow'
+        })
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error))
 }
